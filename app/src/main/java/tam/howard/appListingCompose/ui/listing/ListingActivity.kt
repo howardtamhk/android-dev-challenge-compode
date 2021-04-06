00702a97b3f862f346d039e8543278d49f8e5eca
Howard Tam <howardtamhk@gmail.com>
2021-04-06 16:10:21 +0800

    add missing hilt setup

--- app/src/main/java/tam/howard/appListingCompose/ui/listing/ListingActivity.kt
+++ app/src/main/java/tam/howard/appListingCompose/ui/listing/ListingActivity.kt
@@ -6,9 +6,11 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import tam.howard.appListingCompose.base.BaseActivity
 import tam.howard.appListingCompose.ui.theme.MyTheme
 
+@AndroidEntryPoint
 class ListingActivity : BaseActivity<ListingViewModel>() {
     override val vm: ListingViewModel by viewModels()
 
